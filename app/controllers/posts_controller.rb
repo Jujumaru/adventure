74e94b9ea428cedbd0399af2b41ec2941c5171a0
Julien Bidart <jbidart@ncsu.edu>
2020-09-23 17:47:47 +0200

    Post controller + views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,29 @@
 class PostsController < ApplicationController
-    before_action :set_post, only [:show, edit, :update, :destroy]
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-
+    # GET /posts
+    # get /posts.json
     def index
         @posts = Post.all
     end
 
+    # GET /posts/1
+    # GET /posts/1.json
     def show
     end
 
+    # GET /posts/new
     def new
         @post = Post.new
     end
 
+    # GET /posts/1/edit
+
+    def edit
+    end
+
+    # POST /posts
+    # POST /posts.json
     def create
         @post = Post.new(post_params)
 
@@ -27,6 +38,22 @@ class PostsController < ApplicationController
         end
     end
 
+    # PATCH/PUT /posts/1
+    # PATCH/PUT /posts/1.json
+    def update 
+        respond_to do |format|
+            if @post.update(post_params)
+                fromat.html {redirect_to @post, notice: 'Post successfully updated.'}
+                format.json { render :show, status: :ok, location: @post}
+            else
+                format.html { render :edit}
+                format.json {render json: @post.errors, status: :unprocessable_entity}
+            end
+        end
+    end
+
+    # DELETE /posts/1
+    # DELETE /posts/1.json
     def destroy
         @post.destroy
         respond_to do |format|
@@ -36,10 +63,13 @@ class PostsController < ApplicationController
     end
     
     private
+    # Use callbacks to share common setup or constraints between actions.
         def set_post
             @post = Post.find(params[:id])
         end
 
+
+    # Never trust parameters from the scary internet, only allow the white list through.
         def post_params
             params.fetch(:post, {})
         end
