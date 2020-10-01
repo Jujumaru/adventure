f71c92def2815409a6adb478520bbe9043bbf667
Julien Bidart <jbidart@ncsu.edu>
2020-10-01 17:42:27 +0200

    created session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+      if user && if user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        redirect_to user
+      else
+        flash.now[:danger] = 'This email/password combination does not exist'
+        render 'new'
+    end
+  end
+
+  def new
+  end
 end
