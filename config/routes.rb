728de0cebe5df2caaf80b04139f151e04ed411c5
Julien Bidart <jbidart@ncsu.edu>
2020-10-01 15:44:26 +0200

    User Model + passwords

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'site/home'
 
-  resources :posts
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
+  resources :posts
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
